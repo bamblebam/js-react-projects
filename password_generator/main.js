ffaefa9617226be63426121c59d88659fa86ab25
bamblebam <burhanizrangwala@gmail.com>
2020-07-29 05:11:04 +0530

    grid done

--- password_generator/main.js
+++ password_generator/main.js
@@ -1,5 +1,7 @@
+colors = ['#74ee15', '#000000']
 
 CreateGrid()
+setInterval(ChangeNumbers, 100)
 
 function CreateGrid() {
     let size = 30
@@ -12,7 +14,7 @@ function CreateGrid() {
         for (let j = 0; j < columns; j++) {
             const num = document.createElement('div')
             num.classList.add('num')
-            num.innerHTML = '<span>1</span>'
+            num.innerHTML = `<span class='num-value'>${GetRandomNumber()}</span>`
             numRow.appendChild(num)
         }
         document.body.appendChild(numRow)
@@ -20,5 +22,20 @@ function CreateGrid() {
 }
 
 function GetRandomNumber() {
+    number = ''
+    if (Math.random() > 0.33) {
+        number = 1
+    }
+    else if (Math.random() > 0.66) {
+        number = 0
+    }
+    return number
+}
 
+function ChangeNumbers() {
+    nums = document.querySelectorAll('.num')
+    nums.forEach(num => {
+        num.innerHTML = `<span class='num-value'>${GetRandomNumber()}</span>`
+    });
+    console.log('change')
 }
